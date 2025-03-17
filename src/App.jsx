5020a667daa7d4f09eca0ed00642f10333d98b25
MavenarkAdvisor <tech@mavenark.co.in>
2025-03-17 10:53:51 +0530

    delete excel file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import Excel from './Excel'
 import Speech from './Speech'
 
 const App = () => {
   return (
     <div>
-      {/* <Excel/> */}
       <Speech/>
     </div>
   )
