8b1067dabb5af6737cf011d841b36b46bf2f4b9d
MavenarkAdvisor <tech@mavenark.co.in>
2025-03-19 11:13:18 +0530

    localhost  added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://mvp-backend.gplanck-mavenark.com/",
+      "/api": "http://localhost:8080/",
     },
   },
   plugins: [react()],
